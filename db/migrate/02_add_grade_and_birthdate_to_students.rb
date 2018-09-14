8570e67c92138cf0deccb0f1c50d850e2264287e
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-14 17:12:55 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,7 +2,7 @@ class Add_grade_and_birthdate_to_students < ActiveRecord::Migration[5.1]
 
  def change
        add_column :students, :grade, :integer
-       add_column :students, :part_number, :string
+       add_column :students, :birthdate, :string
  end
 
 end
